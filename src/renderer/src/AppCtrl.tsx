de65ed6750778245128a12885222cc87c972bdfe
Gabriel Gonçalves Martins <gabrielgmartinsbr@gmail.com>
2024-06-24 02:59:59 -0300

    Refactor AppCtrl methods
    Create a class instance reference called methods to
    hold controller component methods and prevent
    them from being recreated on each render.

--- src/renderer/src/AppCtrl.tsx
+++ src/renderer/src/AppCtrl.tsx
@@ -11,6 +11,7 @@ import { useIpcMessage } from './hooks/useIpcMessage';
 import { TerminalMsg, TerminalMsgType } from '@common/ipcMsgs/TerminalMsgs';
 import { useRefSet4 } from './hooks/useRefSet4';
 import { useRefSet3 } from './hooks/useRefSet3';
+import { useInstanceRef } from './hooks/useInstanceRef';
 
 export default function AppCtrl(props: PropsWithChildren) {
     const refs = useRefSet3(class {
@@ -19,96 +20,117 @@ export default function AppCtrl(props: PropsWithChildren) {
     const wrapRefSet = useRefSet4<HTMLDivElement | null>();
     const termRefSet = useRefSet4<Terminal | null>();
     const fitAddonRefSet = useRefSet4<FitAddon | null>();
-    
+
     const [tabs, tabsUpdate] = useImmer<TerminalTab[]>([]);
     const [selectedTab, selectedTabSet] = useState<SelectedTab>(null);
 
-    const createTab = () => {
-        // const newTabUid = nanoid();
-        // tabsUpdate(d => {
-        //     d.push({
-        //         uid: newTabUid
-        //     });
-        // });
-        // selectedTabSet(newTabUid);
-        TerminalMessenger.createTab();
-    };
-
-    const closeTab = (uid: TerminalTabUid) => {
-        // const selectNextTab = (index: number) => {
-        //     let nextTab = tabs[index + 1];
-        //     if (!nextTab) {
-        //         nextTab = tabs[index - 1];
-        //     }
-        //     if (nextTab) {
-        //         selectTab(nextTab.uid);
-        //     }
-        // };
-        // tabsUpdate(d => {
-        //     const index = d.findIndex(i => i.uid === uid);
-        //     if (index > -1) {
-        //         d.splice(index, 1);
-        //         selectNextTab(index);
-        //     }
-        // });
-        TerminalMessenger.closeTab(uid);
-    };
-
-    const selectTab = (uid: TerminalTabUid) => {
-        selectedTabSet(uid);
-    };
-
-    const handleTerminalMsg = (msg: TerminalMsg) => {
-        switch (msg.type) {
-            case TerminalMsgType.TERMINAL_INSTANCES: {
-                handleTabs(msg.tabs);
-                break;
-            }
-            case TerminalMsgType.OUTPUT: {
-                handleOutput(msg.uid, msg.data);
-                break;
-            }
-            default: {
-                console.log(msg);
-            }
-        }
-    };
-
-    const handleTabs = (tabs: TerminalTab[]) => { 
-        refs.tabs = tabs;
-        tabsUpdate(refs.tabs);
-    };
-
-    const handleOutput = (uid: TerminalTabUid, data: string) => {
-        const process = termRefSet.get(uid);
-        if (!process) {
-            throw new Error('Terminal process not found. Process UID: ' + uid);
-        }
-        process.write(data);
-    }
-
-    useEffect(() => {
-        // selectedTabSet(tabs[0].uid);
-    }, []);
-
-    useEffect(() => {
-        TerminalMessenger.fetchTabs();
-    }, []);
-
-
-    useIpcMessage(IpcChannel.TERMINAL, (_ev: unknown, msg: unknown) => {
-        handleTerminalMsg(msg as TerminalMsg);
-    });
-
-    // Handle window resize 
-    useEffect(() => {
-        const handleResize = () => {
+    const methods = useInstanceRef(class {
+        handleResize() {
             fitAddonRefSet.getValuesArray().forEach(v => {
                 v?.fit();
             });
         };
+
+        handleWindowKeyDown(e: KeyboardEvent) {
+            if (!e.shiftKey || !e.ctrlKey) {
+                return;
+            }
+            switch (e.code) {
+                case 'KeyC':
+                    this.requestCopy();
+                    break;
+                case 'KeyV':
+                    this.requestPaste();
+                    break;
+            }
+        }
+
+        handleTerminalMsg = (msg: TerminalMsg) => {
+            switch (msg.type) {
+                case TerminalMsgType.TERMINAL_INSTANCES: {
+                    this.handleTabs(msg.tabs);
+                    break;
+                }
+                case TerminalMsgType.OUTPUT: {
+                    this.handleOutput(msg.uid, msg.data);
+                    break;
+                }
+                default: {
+                    console.log(msg);
+                }
+            }
+        };
+
+        handleTabs(tabs: TerminalTab[]) {
+            refs.tabs = tabs;
+            tabsUpdate(refs.tabs);
+        };
+
+        handleOutput(uid: TerminalTabUid, data: string) {
+            const process = termRefSet.get(uid);
+            if (!process) {
+                throw new Error('Terminal process not found. Process UID: ' + uid);
+            }
+            process.write(data);
+        }
+
+        createTab() {
+            // const newTabUid = nanoid();
+            // tabsUpdate(d => {
+            //     d.push({
+            //         uid: newTabUid
+            //     });
+            // });
+            // selectedTabSet(newTabUid);
+            TerminalMessenger.createTab();
+        };
+
+        closeTab(uid: TerminalTabUid) {
+            // const selectNextTab = (index: number) => {
+            //     let nextTab = tabs[index + 1];
+            //     if (!nextTab) {
+            //         nextTab = tabs[index - 1];
+            //     }
+            //     if (nextTab) {
+            //         selectTab(nextTab.uid);
+            //     }
+            // };
+            // tabsUpdate(d => {
+            //     const index = d.findIndex(i => i.uid === uid);
+            //     if (index > -1) {
+            //         d.splice(index, 1);
+            //         selectNextTab(index);
+            //     }
+            // });
+            TerminalMessenger.closeTab(uid);
+        };
+
+        selectTab(uid: TerminalTabUid) {
+            selectedTabSet(uid);
+        };
+
+        requestCopy() {
+            const tab = refs.tabs[0];
+            const term = termRefSet.get(tab.uid);
+            if (!term) {
+                throw new Error('Selected tab terminal was not found.');
+            }
+            const selection = term.getSelection();
+            TerminalMessenger.requestCopy(tab.uid, selection);
+        }
+
+        requestPaste() {
+            const tab = refs.tabs[0];
+            TerminalMessenger.requestPaste(tab.uid);
+        }
+    }, []);
+
+    useEffect(() => {
+        const handleResize = () => {
+            methods().handleResize();
+        };
         const handleKeyDown = (e: KeyboardEvent) => {
-            handleWindowKeyDown(e);
+            methods().handleWindowKeyDown(e);
         };
         window.addEventListener('resize', handleResize);
         window.addEventListener('keydown', handleKeyDown);
@@ -118,34 +140,17 @@ export default function AppCtrl(props: PropsWithChildren) {
         };
     }, []);
 
-    function handleWindowKeyDown(e: KeyboardEvent) {
-        if (!e.shiftKey || !e.ctrlKey) {
-            return;
-        }
-        switch (e.code) {
-            case 'KeyC':
-                requestCopy();
-                break;
-            case 'KeyV':
-                requestPaste();
-                break;
-        }
-    }
+    useIpcMessage(IpcChannel.TERMINAL, (_ev: unknown, msg: unknown) => {
+        methods().handleTerminalMsg(msg as TerminalMsg);
+    });
 
-    function requestCopy() {
-        const tab = refs.tabs[0];
-        const term = termRefSet.get(tab.uid);
-        if (!term) {
-            throw new Error('Selected tab terminal was not found.');
-        }
-        const selection = term.getSelection();
-        TerminalMessenger.requestCopy(tab.uid, selection);
-    }
+    useEffect(() => {
+        // selectedTabSet(tabs[0].uid);
+    }, []);
 
-    function requestPaste() {
-        const tab = refs.tabs[0];
-        TerminalMessenger.requestPaste(tab.uid);
-    }
+    useEffect(() => {
+        TerminalMessenger.fetchTabs();
+    }, []);
 
     return (
         <appContext.Provider value={{
@@ -154,9 +159,9 @@ export default function AppCtrl(props: PropsWithChildren) {
             wrapRefSet,
             termRefSet,
             fitAddonRefSet,
-            createTab,
-            closeTab,
-            selectTab
+            createTab: methods().createTab,
+            closeTab: methods().closeTab,
+            selectTab: methods().selectTab
         }}>
             {props.children}
         </appContext.Provider>
